d21e74af6ac23a41254634537a48b09e56b9e976
timrockx <timothyyinlee@gmail.com>
2021-07-26 09:51:29 -0400

    setting DEBUG to false manually,  updated detail and overview templates with typo changes

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
-ALLOWED_HOSTS = ['mydjangogym.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mydjangogym.herokuapp.com']
 
 
 # Application definition
