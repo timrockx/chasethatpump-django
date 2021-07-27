77883a0f5b826cb070771fc84a2bffe9c64b44dd
timrockx <timothyyinlee@gmail.com>
2021-07-27 11:37:43 -0400

    adding google analytics functionality, tags in base template and app to installed apps in Settings

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'forum.apps.ForumConfig',
     'crispy_forms',
+    'google_analytics',
     'django_social_share',
     'django.contrib.admin',
     'django.contrib.auth',
