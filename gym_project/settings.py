0ba49dd1c23a39186c5164a3b8f2f9bdae5b7080
timrockx <timothyyinlee@gmail.com>
2021-07-28 10:55:31 -0400

    secret key security fix

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-b(c$i-k9&b$w95aaz2!g40c1wyf7g4-l*@*emv!-keu52aegom'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
